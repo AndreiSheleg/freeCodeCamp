56c153493c74c5724e209305a860ec513f699dcb
Andrei Sheleg <andrej.sheleg@gmail.com>
2023-12-06 00:42:34 +0300

    Create #226 Missing letters.js
    Find the missing letter in the passed letter range and return it.
    
    If all letters are present in the range, return undefined.

---
+++ #226 Missing letters.js
@@ -0,0 +1,14 @@
+function fearNotLetter(str) {
+for (let i = 0; i < str.length - 1; i++) {
+  let currentCharCode = str.charCodeAt(i)
+  let nextCharCode = str.charCodeAt(i + 1)
+
+  if ( nextCharCode - currentCharCode > 1) {
+    let missingLetter = String.fromCharCode(currentCharCode + 1
+    )
+    return missingLetter
+  }
+}
+}
+
+fearNotLetter("abce");
